0d417556ef9415a667393919774842ab99b5e1d9
Yesterday17 <t@yesterday17.cn>
2019-10-10 16:53:12 +0800

    feat: generate test.ass with correct styles

---
+++ src/components/DownloadDialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <md-dialog :md-active.sync="show">
+    <md-dialog-title>下载 ass</md-dialog-title>
+
+    <md-dialog-actions>
+      <md-button class="md-primary" @click="hideDialog">关闭</md-button>
+      <md-button
+        class="md-primary"
+        :href="href"
+        :download="filename"
+        @click="hideDialog"
+        >下载</md-button
+      >
+    </md-dialog-actions>
+  </md-dialog>
+</template>
+
+<script>
+export default {
+  props: ["content"],
+  methods: {
+    showDialog() {
+      this.show = true;
+    },
+    hideDialog() {
+      this.show = false;
+    }
+  },
+  computed: {
+    href() {
+      return URL.createObjectURL(new Blob([this.content]));
+    }
+  },
+  data() {
+    return {
+      show: false,
+      filename: "test.ass"
+    };
+  }
+};
+</script>
