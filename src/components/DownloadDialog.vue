e59402361f6967799fe9a9a297cf83de9b9a9832
Yesterday17 <t@yesterday17.cn>
2019-10-10 17:16:52 +0800

    feat: better download file name

--- src/components/DownloadDialog.vue
+++ src/components/DownloadDialog.vue
@@ -2,6 +2,15 @@
   <md-dialog :md-active.sync="show">
     <md-dialog-title>下载 ass</md-dialog-title>
 
+    <md-field class="ml-download">
+      <label>回数</label>
+      <md-input v-model="kai" type="number"></md-input>
+    </md-field>
+    <div class="ml-download">
+      <md-radio v-model="isOmake" :value="false">本篇</md-radio>
+      <md-radio v-model="isOmake" :value="true">omake</md-radio>
+    </div>
+
     <md-dialog-actions>
       <md-button class="md-primary" @click="hideDialog">关闭</md-button>
       <md-button
@@ -15,6 +24,12 @@
   </md-dialog>
 </template>
 
+<style lang="scss" scoped>
+.ml-download {
+  margin: 0 24px 0;
+}
+</style>
+
 <script>
 export default {
   props: ["content"],
@@ -24,18 +39,28 @@ export default {
     },
     hideDialog() {
       this.show = false;
+    },
+    isContentOmake() {
+      return !this.content.includes("みりっほー");
     }
   },
   computed: {
     href() {
       return URL.createObjectURL(new Blob([this.content]));
+    },
+    filename() {
+      return this.kai.toString() + (this.isOmake ? "omake" : "本篇") + ".ass";
     }
   },
   data() {
     return {
       show: false,
-      filename: "test.ass"
+      isOmake: false,
+      kai: 0
     };
+  },
+  mounted() {
+    this.isOmake = this.isContentOmake();
   }
 };
 </script>
